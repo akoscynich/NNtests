d929ba439ee5f6c24fbf854aab54f8152ac2a324
Anatoly Kiscynich <hamster@node-2.local>
2019-03-19 11:44:48 +0300

    sdfsd

--- src/test/java/SwaggerTests.java
+++ src/test/java/SwaggerTests.java
@@ -1,14 +1,17 @@
 import org.testng.Assert;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.NoInjection;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class SwaggerTests {
+public class SwaggerTests extends _Manager {
 
     @Test
-    public void response() throws IOException {
+    public void swaggerResponse() throws IOException {
         URL url = new URL("https://nsp.mygento.net/ru/rest/all/schema?services=all");
         HttpURLConnection connection = (HttpURLConnection)url.openConnection();
         Assert.assertEquals(connection.getContentType(), "application/json");
--- src/test/java/_Manager.java
+++ src/test/java/_Manager.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
@@ -19,12 +20,13 @@ public class _Manager {
 
     //Fixture--------------------------------------------------------------------------------
 
+
     public void init() {
 
         if (browser.equals(BrowserType.FIREFOX)) {
             wd = new FirefoxDriver();
         } else if (browser.equals(BrowserType.CHROME)) {
-            wd = new ChromeDriver();
+            wd = new ChromeDriver(new ChromeOptions().setHeadless(true));
         } else if (browser.equals(BrowserType.IE)) {
             wd = new InternetExplorerDriver();
         } else if (browser.equals(BrowserType.SAFARI)) {
@@ -32,7 +34,7 @@ public class _Manager {
         }
 
         wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-        wd.get(baseUrl);
+        //wd.get(baseUrl);
     }
 
     public void stop() {
@@ -71,12 +73,12 @@ public class _Manager {
 
     //---------------------------------------------------------------------------------------
 
-    @BeforeSuite(alwaysRun = true)
+    @BeforeSuite//(alwaysRun = true)
     public void setUp() {
         init();
     }
 
-    @AfterSuite(alwaysRun = true)
+    @AfterSuite//(alwaysRun = true)
     public void tearDown() {
         stop();
     }
