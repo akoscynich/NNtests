3d25a8664815a385c4f9ee3c9ce4d3d5814433d7
Anatoly Kiscynich <hamster@node-2.local>
2019-03-29 11:02:36 +0300

    sdfsd

--- src/test/java/SwaggerTests.java
+++ src/test/java/SwaggerTests.java
@@ -11,8 +11,15 @@ public class SwaggerTests {
     public void swaggerResponse() throws IOException {
         URL url = new URL("https://nsp.mygento.net/ru/rest/all/schema?services=all");
         HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-        Assert.assertEquals(connection.getContentType(), "application/json");
         Assert.assertEquals(connection.getResponseCode(), 200);
+        System.out.println(connection.getResponseCode());
+        String contentType = null;
+        if ((connection.getContentType()).equals("application/json"))
+            contentType = "application/json";
+        if ((connection.getContentType()).equals("application/json; charset=utf-8"))
+            contentType = "application/json; charset=utf-8";
+        System.out.println(contentType);
+        Assert.assertEquals(connection.getContentType(), contentType);
     }
 
 }
